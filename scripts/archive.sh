4301e8cc07d994263f9dc2f524d0c89a0dcf4da2
Jeremy Falcon <jfalcon@users.noreply.github.com>
2025-06-19 17:47:04 -0500

    feat: zip

---
+++ scripts/archive.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# check for argument
+if [ -z "$1" ]; then
+  echo "Usage: $0 output_filename"
+  exit 1
+fi
+
+OUT_FILE="$1.zip"
+PARENT_DIR="$(dirname "$(dirname "$(realpath "$0")")")"
+
+# ensure the output file doesn't already exist
+if [ -f "$OUT_FILE" ]; then
+  echo "Error: File '$OUT_FILE' already exists. Please choose a different name."
+  exit 2
+fi
+
+# avoid including zip files (if rerun)
+cd "$PARENT_DIR" > /dev/null 2>&1
+zip -r "$OUT_FILE" . -x "./*.zip" "./.git/*" "./dist/*" "./node_modules/*"
+cd - > /dev/null 2>&1
