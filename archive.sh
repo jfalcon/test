5bb1849a6f6f3595707fd1181cbaf7ebc1c00865
Jeremy Falcon <jfalcon@users.noreply.github.com>
2025-06-19 17:47:04 -0500

    refactor: scripts

--- archive.sh
+++ archive.sh
@@ -7,15 +7,17 @@ if [ -z "$1" ]; then
 fi
 
 OUT_FILE="$1.zip"
-PARENT_DIR="$(dirname "$(dirname "$(realpath "$0")")")"
+CURRENT_DIR="$(dirname "$(realpath "$0")")"
+
+cd "$PARENT_DIR" > /dev/null 2>&1
 
 # ensure the output file doesn't already exist
 if [ -f "$OUT_FILE" ]; then
   echo "Error: File '$OUT_FILE' already exists. Please choose a different name."
-  exit 2
+  cd - > /dev/null 2>&1
+  exit 1
 fi
 
 # avoid including zip files (if rerun)
-cd "$PARENT_DIR" > /dev/null 2>&1
 zip -r "$OUT_FILE" . -x "./*.zip" "./.git/*" "./.vscode/*" "./dist/*" "./node_modules/*"
 cd - > /dev/null 2>&1
