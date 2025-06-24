87d96c748ad5aa3ba8122408734a0c762e6f251c
Jeremy Falcon <jfalcon@users.noreply.github.com>
2025-06-23 20:12:13 -0500

    test: p2p

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,11 +1,15 @@
-// src/components/ThemeToggle.tsx
 import React, { useState, useEffect } from 'react';
 
 type ThemeMode = 'light' | 'dark';
-const defaultTheme = (localStorage.getItem('theme') ?? 'light') as ThemeMode;
+
+const defaultTheme = (
+  window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
+) ? 'dark' : 'light';
+
+const initTheme = (localStorage.getItem('theme') ?? defaultTheme) as ThemeMode;
 
 const ThemeToggle: React.FC = () => {
-  const [theme, setTheme] = useState<ThemeMode>(defaultTheme);
+  const [theme, setTheme] = useState<ThemeMode>(initTheme);
 
   // Load saved theme from localStorage on component mount
   useEffect(() => {
