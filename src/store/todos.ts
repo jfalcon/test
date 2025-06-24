dcfa7d2593aa4eed0ba7d17134bf78ec31fef9fc
Jeremy Falcon <jfalcon@users.noreply.github.com>
2025-06-23 23:49:32 -0500

    feat: todos

--- src/store/todos.ts
+++ src/store/todos.ts
@@ -13,7 +13,7 @@ export const STORAGE_KEY = 'todos';
 // not using enums for this project
 export type FilterStatus = 'ALL' | 'ACTIVE' | 'COMPLETED';
 
-interface TodoState {
+export interface TodoState {
   todos: Todo[];
   filter: FilterStatus;
 }
@@ -21,6 +21,32 @@ interface TodoState {
 const initialState: TodoState = {
   todos: [],
   filter: 'ALL', // not using enums for this project
+};
+
+export const loadTodos = (): Todo[] => {
+  try {
+    const serialized = localStorage.getItem(STORAGE_KEY);
+    const parsed: Partial<Todo[]> = serialized ? JSON.parse(serialized) : [];
+
+    // if the data was corrupted then ensure at least the shape is restored
+    return parsed.map(p => {
+      return {
+        id: p?.id ?? 0,
+        text: p?.text ?? '',
+        completed: p?.completed ?? false,
+        count: p?.count ?? 0,
+      };
+    });
+  } catch {
+    return [];
+  }
+};
+
+export const saveTodos = (todos: Todo[]) => {
+  try {
+    const serialized = JSON.stringify(todos);
+    localStorage.setItem(STORAGE_KEY, serialized);
+  } catch {} // ignore write errors
 };
 
 export const todoSlice = createSlice({
