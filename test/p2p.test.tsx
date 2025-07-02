e7bc414501a0c1030919dec62c3765d75e35cbc9
Jeremy Falcon <jfalcon@users.noreply.github.com>
2025-07-02 09:16:31 -0500

    feat: p2p

--- test/p2p.test.tsx
+++ test/p2p.test.tsx
@@ -1,6 +1,8 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import { store } from '../src/store';
+import { setupStore, store } from '../src/store';
+import type { RootState } from '../src/store';
+import { STORAGE_KEY } from '../src/store/todos';
 
 describe('P2P Tests', () => {
   afterEach(() => {
@@ -65,6 +67,47 @@ describe('P2P Tests', () => {
       expect(count).toStrictEqual(3);
     });
   })
+
+  describe('Local Storage', () => {
+    beforeEach(() => localStorage.clear());
+
+    it("loads todos from localStorage", () => {
+      const fakeState: RootState = {
+        todos: {
+          todos: [{
+            id: 0,
+            text: 'task 1',
+            completed: false,
+            count: 0,
+          }, {
+            id: 1,
+            text: 'task 2',
+            completed: false,
+            count: 0,
+          }],
+          filter: 'ALL',
+        }
+      };
+
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(fakeState));
+
+      // since this mocked we don't need to map like we do with production
+      const stored = localStorage.getItem(STORAGE_KEY);
+      const parsed: RootState = stored ? JSON.parse(stored) : undefined;
+
+      const store = setupStore(parsed);
+
+      const state = store.getState();
+      expect(state.todos.todos).toStrictEqual(fakeState.todos.todos);
+    });
+
+    it("safely initializes todos when localStorage is empty", () => {
+      const store = setupStore(undefined);
+      const state = store.getState();
+
+      expect(state.todos.todos).toEqual([]);
+    });
+  });
 
   describe('Theme Mode Toggle', () => {
     const originalMatchMedia = window.matchMedia;
