872f1fb1ea71d4e269e8fe8bace6430d5f9e30da
Jeremy Falcon <jfalcon@users.noreply.github.com>
2025-07-13 12:31:46 -0500

    feat: panel

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { DIGITS } from '@/constants';
 import type { RootState } from '@/store';
@@ -8,11 +8,137 @@ type PanelProps = {
 };
 
 const Panel: React.FC<PanelProps> = () => {
+  const [useLots, setUseLots] = useState(false);
   const trueRange = useSelector((state: RootState) => state.meta.trueRange);
 
   return (
     <section id="panel" data-testid="panel">
-      True Range: {trueRange && trueRange.toFixed(DIGITS)}
+      <div>True Range: {trueRange && trueRange.toFixed(DIGITS)}</div>
+      <div className="panes">
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th colSpan={4}>Main Header</th>
+              </tr>
+              <tr>
+                <th>Column 1</th>
+                <th>Column 2</th>
+                <th>Column 3</th>
+                <th>Column 4</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+              </tr>
+              <tr>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+              </tr>
+              <tr>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+              </tr>
+              <tr>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+              </tr>
+              <tr>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+              </tr>
+              <tr>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+              </tr>
+            </tbody>
+            <tfoot>
+              <tr>
+                <td>Totals</td>
+                <td>Totals</td>
+                <td>Totals</td>
+                <td>Totals</td>
+              </tr>
+            </tfoot>
+          </table>
+        </div>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th colSpan={4}>Main Header</th>
+              </tr>
+              <tr>
+                <th>Column 1</th>
+                <th>Column 2</th>
+                <th>Column 3</th>
+                <th>Column 4</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+              </tr>
+              <tr>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+              </tr>
+              <tr>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+              </tr>
+              <tr>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+              </tr>
+              <tr>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+              </tr>
+              <tr>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+              </tr>
+            </tbody>
+            <tfoot>
+              <tr>
+                <td>Totals</td>
+                <td>Totals</td>
+                <td>Totals</td>
+                <td>Totals</td>
+              </tr>
+            </tfoot>
+          </table>
+        </div>
+      </div>
+      <div>Balance: 42</div>
     </section>
   );
 };
