90ccfbae64b024f1f075c01d6e108fc83981b7c7
Jeremy Falcon <jfalcon@users.noreply.github.com>
2025-07-17 08:29:27 -0500

    feat: tick

--- server/app/routes/price.ts
+++ server/app/routes/price.ts
@@ -3,7 +3,6 @@ import * as priceControllers from '@/controllers/price';
 
 const priceRouter = Router();
 
-// showing an index example with barrel imports
 priceRouter.get('/', priceControllers.index);
 
 export default priceRouter;
